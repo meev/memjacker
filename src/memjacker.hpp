7a6b156229aa3393bcc6a5ec78032af96aa5d575
meev <alex@vestfris.dk>
2016-10-10 20:23:34 +0200

    Removal of some stuff

--- src/memjacker.hpp
+++ src/memjacker.hpp
@@ -3,12 +3,6 @@
 
 #define HOOK_FIND_CALLING_CONVENTION		0x0
 
-enum hook_architecture_e
-{
-	HOOK_ARCHITECTURE_I386 = 0,
-	HOOK_ARCHITECTURE_X86_64,
-};
-
 enum hook_safety_level_e
 {
 	HOOK_SAFETY_CLEAN = 0,
